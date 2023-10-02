5b9cf05b42a9ca136c1f44e2dfbaa01f53e67dbf
thuwainik <142627387+thuwainik@users.noreply.github.com>
2023-10-03 01:45:58 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import kha from "./queb.png";
 import "./App.css";
+import products from "./products";
 
+let productList = products.map((p) => {
+  return (
+    <div className="product">
+      <img src={p.img} alt="ccc"></img>
+      <h4>{p.name}</h4>
+      <h4>{p.price}</h4>
+    </div>
+  );
+});
 function App() {
   return (
-    <div>
-      <h1 className="title">QueB</h1>
-      <h4 className="slogan">Mix Shape & Bake</h4>
-      <img src={kha} className="qb"></img>
-    </div>
+    <>
+      <div>
+        <h1 className="title">QueB</h1>
+        <h4 className="slogan">Mix Shape & Bake</h4>
+        <img src={kha} alt="QB icon" className="qb"></img>
+      </div>
+      <div className="container">{productList}</div>
+    </>
   );
 }
 
---
+++ src/products.js
@@ -0,0 +1,15 @@
+import ccc from "./CCC.png";
+import dc from "./DC.png";
+let products = [
+  {
+    name: "Chocolate-Chip Cookies (10pcs)",
+    price: 4.5,
+    img: ccc,
+  },
+  {
+    name: "Dark Cookies (10 pcs)",
+    price: 4.5,
+    img: dc,
+  },
+];
+export default products;
